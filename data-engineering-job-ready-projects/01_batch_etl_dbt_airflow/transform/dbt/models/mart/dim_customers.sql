4a18b38695922aebc99cbca2c9a77faee145884a
Jamie Christian II <jamiechristian557@gmail.com>
2025-12-17 22:38:26 -0500

    Delete data-engineering-job-ready-projects/01_batch_etl_dbt_airflow directory

--- data-engineering-job-ready-projects/01_batch_etl_dbt_airflow/transform/dbt/models/mart/dim_customers.sql
+++
@@ -1,9 +0,0 @@
-select
-  c.customer_id,
-  c.created_date,
-  c.state,
-  c.city,
-  c.email_domain,
-  -- customer tenure in days
-  (current_date - c.created_date) as tenure_days
-from {{ ref('stg_customers') }} c
