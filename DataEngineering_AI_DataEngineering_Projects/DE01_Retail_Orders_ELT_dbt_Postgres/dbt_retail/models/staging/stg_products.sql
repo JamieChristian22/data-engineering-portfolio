cf9db62a933a6bd0d6bd617e9f9c758818f8795c
Jamie Christian II <jamiechristian557@gmail.com>
2026-01-25 16:37:35 -0500

    Delete DataEngineering_AI_DataEngineering_Projects directory

--- DataEngineering_AI_DataEngineering_Projects/DE01_Retail_Orders_ELT_dbt_Postgres/dbt_retail/models/staging/stg_products.sql
+++
@@ -1,7 +0,0 @@
-select
-  sku,
-  category,
-  brand,
-  unit_cost::numeric(12,2) as unit_cost,
-  unit_price::numeric(12,2) as unit_price
-from raw.products
--- DataEngineering_AI_DataEngineering_Projects/tests/test_imports.py
+++
@@ -1,2 +0,0 @@
-def test_import_shared():
-    import shared.utils  # noqa
